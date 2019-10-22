ba6ee905f41e877828b0216020eac47953196fb4
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-22 19:42:51 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,1 @@
-Trace.Write("Hello Opening Public GIT Master") 
-checks = ["Access", "Reach", "Authenticated", "Data Provided"]
-for x in checks:
-  Trace.Write(x) 
-Trace.Write("Test Success")
+Trace.Write("Hello Opening Public GIT Master")
