d23b60cb68e889214f120053afa0e2891067c470
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-23 13:49:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-Trace.Write("Hello Open GIT Master")
-checks = ["Access", "Reach", "Authenticated", "Data Provided"]
-for x in checks:
+Trace.Write("Hello Open GIT Master") 
+for x in ["Access", "Reach", "Authenticated", "Data Provided"]:
   Trace.Write(x) 
 Trace.Write("Success")
