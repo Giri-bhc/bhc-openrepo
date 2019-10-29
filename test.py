5c7ccaf4069d2fd9ff6904864ea8c2a6bbb3e9a3
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-29 16:59:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-'''
 Trace.Write("Hello Open GIT Master")
 for x in range(1,5):
- Trace.Write(x)
- Trace.Write(x)
+  Trace.Write(x) 
 Trace.Write("Success")
-'''
-
-print("Hello Open GIT Master")
-for x in range(1,5):
- print(x)
- print(x)
-print("Success")
