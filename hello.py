bcc885dde6c6fd4adbd45aec11023687685019b7
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-29 17:45:34 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,10 @@
 import clr
 clr.AddReference("System.Net")
+clr.AddReference("Scripting")
 from System.Net import CookieContainer
 from System.Net import Cookie
 from System.Net import WebRequest
 from System.Net import HttpWebResponse
-from System.Diagnostics import Trace
 
 test = Product.Attr('SOL_NeedMaX').GetValue()
 
-Trace.Write("Hello, world")
-a = 0
-for x in range(1,5):
-  a = a + 1
-Trace.Write("Success")
