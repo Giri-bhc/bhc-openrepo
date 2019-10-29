d842147e36a0b4a1b43f6dae76608901b15e10c6
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-29 18:06:57 +0530

    Update hello.py (#16)

--- hello.py
+++ hello.py
@@ -1,11 +1,6 @@
 import clr
 clr.AddReference("System.Net")
 clr.AddReference("Scripting")
-from System.Net import CookieContainer
-from System.Net import Cookie
-from System.Net import WebRequest
-from System.Net import HttpWebResponse
-import Scripting
 
-test = Scripting.Product.Attr('SOL_NeedMaX').GetValue()
+test = Product.Attributes.GetByName('SOL_NeedMaX')
 
