198071602ca22290a9a86308b4845e256a209179
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-29 13:07:45 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
+import clr
+clr.AddReference("System.Net")
+from System.Net import CookieContainer
+from System.Net import Cookie
+from System.Net import WebRequest
+from System.Net import HttpWebResponse
+
 Trace.Write("Hello Open GIT Master")
 for x in range(1,5): 
   Trace.Write(x)
