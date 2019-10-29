76a3cdc4d865ec341409bc33c0f4553efb304a14
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-29 13:59:51 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
-
-for x in range(1,5): 
+import clr
+clr.AddReference("System.Net")
+from System.Net import CookieContainer
+from System.Net import Cookie
+from System.Net import WebRequest
+from System.Net import HttpWebResponse
+    print "Hello, world"
+for x in range(1,5):
   print x
 print "Success"
