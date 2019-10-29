fe30a25d3a30e1c31fc96bc044c81d694ed7bdae
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-29 13:11:19 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,4 @@
-import clr
-clr.AddReference("System.Net")
-from System.Net import CookieContainer
-from System.Net import Cookie
-from System.Net import WebRequest
-from System.Net import HttpWebResponse
-
-Trace.Write("Hello Open GIT Master")
+print "Hello Open GIT Master"
 for x in range(1,5): 
-  Trace.Write(x)
-Trace.Write("Success")
+  print x
+print "Success"
