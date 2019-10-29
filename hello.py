c0df5d17637ba20928227ec17fad9b41444f07d6
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-29 14:03:48 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,8 @@ from System.Net import CookieContainer
 from System.Net import Cookie
 from System.Net import WebRequest
 from System.Net import HttpWebResponse
-    print "Hello, world"
+    
+print "Hello, world"
 for x in range(1,5):
   print x
 print "Success"
