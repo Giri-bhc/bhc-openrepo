421fad779466bc8e645b3794b9bc50e897837dec
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-10-29 17:49:54 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ from System.Net import CookieContainer
 from System.Net import Cookie
 from System.Net import WebRequest
 from System.Net import HttpWebResponse
+from Scripting import Product
 
 test = Product.Attr('SOL_NeedMaX').GetValue()
 
