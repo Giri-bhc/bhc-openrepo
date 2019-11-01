4fcbc9e37b4cb01be672980e04c01f9e7ac5dee8
Giri-bhc <56724120+Giri-bhc@users.noreply.github.com>
2019-11-01 13:06:40 +0530

    Update test_git_2.py

--- test_git_2.py
+++ test_git_2.py
@@ -1,3 +1,3 @@
-Product.Attributes.GetByName('test_011119_attri').AssignValue('test_sample')
+Product.Attributes.GetByName('test_011119_attri').SelectDisplayValue('test_011119_product')
 Param='test_multiscript'
 ScriptExecutor.Execute('test_git', Param)
